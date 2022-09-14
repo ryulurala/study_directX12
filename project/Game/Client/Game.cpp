feb082451fc48dc5a4735ab71503ddd927808d4f
ryulurala <ryulurala@gmail.com>
2022-09-14 21:49:36 +0900

    :sparkles: Index Buffer

--- project/Game/Client/Game.cpp
+++ project/Game/Client/Game.cpp
@@ -14,14 +14,31 @@ void Game::Init(const WindowInfo& window)
 {
 	GEngine->Init(window);
 
-	vector<Vertex> vec(3);
-	vec[0].pos = Vec3(0.f, 0.5f, 0.5f);
-	vec[0].color = Vec4(1.f, 0.f, 0.f, 1.f);	// Red
-	vec[1].pos = Vec3(0.5f, -0.5f, 0.5f);
-	vec[1].color = Vec4(0.f, 1.f, 0.f, 1.f);	// Green
-	vec[2].pos = Vec3(-0.5f, -0.5f, 0.5f);
-	vec[2].color = Vec4(0.f, 0.f, 1.f, 1.f);	// Blue
-	mesh->Init(vec);
+	// Vertex buffer
+	vector<Vertex> vertexVec(4);
+	vertexVec[0].pos = Vec3(-0.5f, 0.5f, 0.5f);
+	vertexVec[0].color = Vec4(1.f, 0.f, 0.f, 1.f);
+	vertexVec[1].pos = Vec3(0.5f, 0.5f, 0.5f);
+	vertexVec[1].color = Vec4(0.f, 1.f, 0.f, 1.f);
+	vertexVec[2].pos = Vec3(0.5f, -0.5f, 0.5f);
+	vertexVec[2].color = Vec4(0.f, 0.f, 1.f, 1.f);
+	vertexVec[3].pos = Vec3(-0.5f, -0.5f, 0.5f);
+	vertexVec[3].color = Vec4(0.f, 1.f, 0.f, 1.f);
+
+	// Index buffer
+	vector<uint32> indexVec;
+	{
+		indexVec.push_back(0);
+		indexVec.push_back(1);
+		indexVec.push_back(2);
+	}
+	{
+		indexVec.push_back(0);
+		indexVec.push_back(2);
+		indexVec.push_back(3);
+	}
+
+	mesh->Init(vertexVec, indexVec);
 
 	shader->Init(L"..\\Resources\\Shader\\default.hlsli");
 
@@ -34,21 +51,10 @@ void Game::Update()
 
 	shader->Update();
 
-	{
-		Transform t;
-		t.offset = Vec4(0.75f, 0.f, 0.f, 0.f);
-		mesh->SetTransform(t);
-
-		mesh->Render();
-	}
-
-	{
-		Transform t;
-		t.offset = Vec4(0.f, 0.75f, 0.f, 0.f);
-		mesh->SetTransform(t);
-
-		mesh->Render();
-	}
+	Transform t;
+	t.offset = Vec4(0.f, 0.f, 0.f, 0.f);
+	mesh->SetTransform(t);
+	mesh->Render();
 
 	//GEngine->Render();
 	
--- project/Game/Engine/Mesh.cpp
+++ project/Game/Engine/Mesh.cpp
@@ -7,20 +7,46 @@
 #include "ConstantBuffer.h"
 #include "TableDescriptorHeap.h"
 
-void Mesh::Init(vector<Vertex>& vec)
+void Mesh::Init(const vector<Vertex>& vertexBuffer, const vector<uint32>& indexBuffer)
 {
-	_vertexCount = static_cast<uint32>(vec.size());		// for. Render
+	CreateVertexBuffer(vertexBuffer);
+	CreateIndexBuffer(indexBuffer);
+}
+
+void Mesh::Render()
+{
+	CMD_LIST->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	CMD_LIST->IASetVertexBuffers(0, 1, &_vertexBufferView);	// Slot: (0~15)
+	CMD_LIST->IASetIndexBuffer(&_indexBufferView);
+
+	// 1) Buffer에 Data 설정
+	D3D12_CPU_DESCRIPTOR_HANDLE handle = GEngine->GetConstantBuffer()->PushData(0, &_transform, sizeof(_transform));
+		
+	// 2) Table Descriptor Heap에 CBV 전달
+	GEngine->GetTableDescHeap()->SetCBV(handle, CBV_REGISTER::b0);
+
+	// 3) Table Descriptor Heap 커밋
+	// GPU Registers가 사용할 수 있도록 올리는 작업
+	GEngine->GetTableDescHeap()->CommitTable();
+
+	//CMD_LIST->DrawInstanced(_vertexCount, 1, 0, 0);		// Vertex 이용
+	CMD_LIST->DrawIndexedInstanced(_indexCount, 1, 0, 0, 0);
+}
+
+void Mesh::CreateVertexBuffer(const vector<Vertex>& buffer)
+{
+	_vertexCount = static_cast<uint32>(buffer.size());		// for. Render
 	uint32 bufferSize = _vertexCount * sizeof(Vertex);
 
 	D3D12_HEAP_PROPERTIES heapProperty = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
 	D3D12_RESOURCE_DESC desc = CD3DX12_RESOURCE_DESC::Buffer(bufferSize);
 
 	DEVICE->CreateCommittedResource(
-		&heapProperty, 
-		D3D12_HEAP_FLAG_NONE, 
-		&desc, 
-		D3D12_RESOURCE_STATE_GENERIC_READ, 
-		nullptr, 
+		&heapProperty,
+		D3D12_HEAP_FLAG_NONE,
+		&desc,
+		D3D12_RESOURCE_STATE_GENERIC_READ,
+		nullptr,
 		IID_PPV_ARGS(&_vertexBuffer)
 	);
 
@@ -28,7 +54,7 @@ void Mesh::Init(vector<Vertex>& vec)
 	void* vertexDataBuffer = nullptr;
 	CD3DX12_RANGE readRange(0, 0);
 	_vertexBuffer->Map(0, &readRange, &vertexDataBuffer);
-	::memcpy(vertexDataBuffer, &vec[0], bufferSize);
+	::memcpy(vertexDataBuffer, &buffer[0], bufferSize);
 	_vertexBuffer->Unmap(0, nullptr);
 
 	// Vertex Buffer View 초기화
@@ -37,27 +63,30 @@ void Mesh::Init(vector<Vertex>& vec)
 	_vertexBufferView.SizeInBytes = bufferSize;			// 버퍼의 크기
 }
 
-void Mesh::Render()
+void Mesh::CreateIndexBuffer(const vector<uint32>& buffer)
 {
-	CMD_LIST->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-	CMD_LIST->IASetVertexBuffers(0, 1, &_vertexBufferView);	// Slot: (0~15)
+	_indexCount = static_cast<uint32>(buffer.size());
+	uint32 bufferSize = _indexCount * sizeof(uint32);
 
-	{
-		// 1) Buffer에 Data 설정
-		D3D12_CPU_DESCRIPTOR_HANDLE handle = GEngine->GetConstantBuffer()->PushData(0, &_transform, sizeof(_transform));
-		
-		// 2) Table Descriptor Heap에 CBV 전달
-		GEngine->GetTableDescHeap()->SetCBV(handle, CBV_REGISTER::b0);
-	}
+	D3D12_HEAP_PROPERTIES heapProperty = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
+	D3D12_RESOURCE_DESC desc = CD3DX12_RESOURCE_DESC::Buffer(bufferSize);
 
-	{
-		D3D12_CPU_DESCRIPTOR_HANDLE handle = GEngine->GetConstantBuffer()->PushData(0, &_transform, sizeof(_transform));
-		GEngine->GetTableDescHeap()->SetCBV(handle, CBV_REGISTER::b1);
-	}
+	DEVICE->CreateCommittedResource(
+		&heapProperty,
+		D3D12_HEAP_FLAG_NONE,
+		&desc,
+		D3D12_RESOURCE_STATE_GENERIC_READ,
+		nullptr,
+		IID_PPV_ARGS(&_indexBuffer)
+	);
 
-	// 3) Table Descriptor Heap 커밋
-	// GPU Registers가 사용할 수 있도록 올리는 작업
-	GEngine->GetTableDescHeap()->CommitTable();
-
-	CMD_LIST->DrawInstanced(_vertexCount, 1, 0, 0);
+	void* IndexDataBuffer = nullptr;
+	CD3DX12_RANGE readRange(0, 0);
+	_indexBuffer->Map(0, &readRange, &IndexDataBuffer);
+	::memcpy(IndexDataBuffer, &buffer[0], bufferSize);
+	_indexBuffer->Unmap(0, nullptr);
+	
+	_indexBufferView.BufferLocation = _indexBuffer->GetGPUVirtualAddress();
+	_indexBufferView.Format = DXGI_FORMAT_R32_UINT;		// 32 bits
+	_indexBufferView.SizeInBytes = bufferSize;
 }
